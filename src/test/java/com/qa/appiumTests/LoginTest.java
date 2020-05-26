8aecf2f157f53ff7d7e3d2e4204babbdee2c7146
mudassir <mhasnain8271@gmail.com>
2020-05-26 18:24:11 +0530

    my first commit

--- src/test/java/com/qa/appiumTests/LoginTest.java
+++ src/test/java/com/qa/appiumTests/LoginTest.java
@@ -3,6 +3,8 @@ package com.qa.appiumTests;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
@@ -12,21 +14,20 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class LoginTest {
 	
-public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException {
 		
 		//Set the Desired Capabilities
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("deviceName", "Pixel XL API 24");
-		caps.setCapability("udid", "ENUL6303030010"); //Give Device ID of your mobile phone
 		caps.setCapability("platformName", "Android");
 		caps.setCapability("platformVersion", "7.0");
 		caps.setCapability("browserName", "Chrome");
 		caps.setCapability("noReset", true);
 		
 		//Set ChromeDriver location
-		System.setProperty("webdriver.chrome.driver","C:\\selenium_drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
 		
-		//Instantiate Appium Driver
+		//Initialize Appium Driver
 		AppiumDriver<MobileElement> driver = null;
 		try {
 			driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
@@ -35,8 +36,12 @@ public static void main(String[] args) {
 			System.out.println(e.getMessage());
 		}
 				
-		//Open URL in Chrome Browser
-		driver.get("http://www.google.com");
+		//Launch URL in Chrome Browser
+		driver.get("https://ess-staging.ttconline.com/");
+		Thread.sleep(1000);
+		driver.findElement(By.id("username")).sendKeys("TestUserName");
+		driver.findElement(By.id("password")).sendKeys("TestPassword");
+		driver.findElement(By.id("button")).click();
 	} 
 
 }
